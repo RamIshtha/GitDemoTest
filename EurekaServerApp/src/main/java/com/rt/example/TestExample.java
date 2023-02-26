0aaf511c836264a02f591cf308183bd4594336a6
jyoti <TL@gmail.com>
2023-02-27 00:12:08 +0530

    aaf

--- EurekaServerApp/src/main/java/com/rt/example/TestExample.java
+++ EurekaServerApp/src/main/java/com/rt/example/TestExample.java
@@ -5,6 +5,7 @@ public class TestExample {
 	public static void main(String[] args) {
 		int i=0;
 		System.out.println("i am lerning git...");
+		System.out.println("i am lerning git...");
 	}
 	
 	public void m1() {
