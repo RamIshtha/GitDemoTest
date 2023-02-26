5cc51e66854318c752475e7253bef60336be02d8
jyoti <TL@gmail.com>
2023-02-27 00:08:29 +0530

    sdas

--- EurekaServerApp/src/main/java/com/rt/example/TestExample.java
+++ EurekaServerApp/src/main/java/com/rt/example/TestExample.java
@@ -3,7 +3,7 @@ package com.rt.example;
 public class TestExample {
 
 	public static void main(String[] args) {
-		System.out.println("wdtyuytasd");
+		int i=0;
 		System.out.println("i am lerning git...");
 	}
 	
