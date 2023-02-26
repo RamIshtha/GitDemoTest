6da260f2e5df123111ef99c17489f59bd8294c97
jyoti <TL@gmail.com>
2023-02-27 00:13:32 +0530

    asds

--- EurekaServerApp/src/main/java/com/rt/example/TestExample.java
+++ EurekaServerApp/src/main/java/com/rt/example/TestExample.java
@@ -3,10 +3,7 @@ package com.rt.example;
 public class TestExample {
 
 	public static void main(String[] args) {
-		System.out.println("i am lerning git...");
 		int i=0;
-		System.out.println("i am lerning git...");
-		System.out.println("i am lerning git...");
 	}
 	
 	public void m1() {
