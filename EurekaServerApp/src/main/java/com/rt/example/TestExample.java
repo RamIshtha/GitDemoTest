dbff7e9ae2533e22436e66d7d05c574a7d136c7a
jyoti <TL@gmail.com>
2023-02-27 07:06:52 +0530

    vgjhgj

--- EurekaServerApp/src/main/java/com/rt/example/TestExample.java
+++ EurekaServerApp/src/main/java/com/rt/example/TestExample.java
@@ -5,10 +5,5 @@ public class TestExample {
 	public static void main(String[] args) {
 		int i=11;
 	}
-	
-	public void m1() {
-		System.out.println("wdtyuytasd");
-		System.out.println("wdtyuytasd");
-	}
 
 }
